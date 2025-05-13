2569aafcd2cb4a3a30b37cd9908c74b96a85a5bf
paronian.nv <paronian.nv@dozen.mephi.ru>
2025-05-13 22:42:52 +0300

    final commit

--- fourth_prog/main.c
+++ fourth_prog/main.c
@@ -11,41 +11,41 @@ int main(int argc, char *argv[]){
 
     while((opt = getopt_long(argc, argv, "mcste:h", long_option, NULL)) != -1){
         switch(opt){
-	    	case 'm':
-				mcst[mcst_cnt++] = 'm';
-				break;
-	    	case 'c':
-				mcst[mcst_cnt++] = 'c';
-				break;
-	    	case 's':
-				mcst[mcst_cnt++] = 's';
-				break;
-	    	case 't':
-				mcst[mcst_cnt++] = 't';
-				break;
-	    	case 'e':
-				if(!elbrus_valid(optarg)){
-		    		printf("Error with elbrus arguments.\n");
-		    		free(elbrus_val);
-		    		return 1;
-				}
-				if(elbrus_cnt>=elbrus_base_size){
-					elbrus_base_size *= 2;
-					char **tmp = realloc(elbrus_val, elbrus_base_size*sizeof(char *));
-					check_alloc(tmp);
-					elbrus_val = tmp;
-				}
-						elbrus_val[elbrus_cnt++] = optarg;
-				break;
-				case 'h':
-				help();
-					free(elbrus_val);
-				return 0;
-				case '?':
-				report = 0;
-				break;
-				default:
-				report = 0;
+			case 'm':
+			mcst[mcst_cnt++] = 'm';
+			break;
+			case 'c':
+			mcst[mcst_cnt++] = 'c';
+			break;
+			case 's':
+			mcst[mcst_cnt++] = 's';
+			break;
+			case 't':
+			mcst[mcst_cnt++] = 't';
+			break;
+			case 'e':
+			if(!elbrus_valid(optarg)){
+		    	printf("Error with elbrus arguments.\n");
+		    	free(elbrus_val);
+		    	return 1;
+			}
+			if(elbrus_cnt>=elbrus_base_size){
+				elbrus_base_size *= 2;
+				char **tmp = realloc(elbrus_val, elbrus_base_size*sizeof(char *));
+				check_alloc(tmp);
+				elbrus_val = tmp;
+			}
+			elbrus_val[elbrus_cnt++] = optarg;
+			break;
+			case 'h':
+			help();
+			free(elbrus_val);
+			return 0;
+			case '?':
+			report = 0;
+			break;
+			default:
+			report = 0;
 		}
 	}
     if(!report){
@@ -60,7 +60,7 @@ int main(int argc, char *argv[]){
     }
 
     for(i = 0; i < elbrus_cnt; i++){
-    	printf("elbrus=%s%s", elbrus_val[i], (i == elbrus_cnt-1) ? "":", ");
+		printf("elbrus=%s%s", elbrus_val[i], (i == elbrus_cnt-1) ? "":", ");
     }
 	
     printf(" ");
