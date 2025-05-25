42fd65496032dcdd58f29c27846af30405efb922
paronian.nv <paronian.nv@dozen.mephi.ru>
2025-05-25 10:19:17 +0300

    add errlist

--- fifth_prog/main.c
+++ fifth_prog/main.c
@@ -1,11 +1,10 @@
-#ifndef __GNU__
 #include<stdio.h>
 #include<errno.h>
 #include<fcntl.h>
 #include<unistd.h>
 
-extern const char* const sys_errlist[];
-extern int sys_nerr;
+extern int _sys_nerr;
+extern __const char *__const _sys_errlist[];
 
 int main(){
     int fd = open("nonexist.txt", O_RDWR);
@@ -13,11 +12,10 @@ int main(){
     if(fd == -1){
         printf("Erroe number: %d\n", errno);
         perror("Error opening file");
-        printf("Errlist: %s", sys_errlist[errno]);
+        printf("Errlist: %s", _sys_errlist[errno]);
     }else{
-    printf("Filr somefile.txt opend seccessufully\nFile descriptor: %d\n", fd);
+    printf("File somefile.txt opend seccessufully\nFile descriptor: %d\n", fd);
     close(fd);
     }
     return 0;
 }
-#endif
