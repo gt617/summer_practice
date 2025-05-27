1f5e4e08a958e465241404f0d907cbcdb0219202
gt617 <tamagohi14@gmail.com>
2025-05-27 17:21:40 +0300

    edits

--- second_prog/main.c
+++ second_prog/main.c
@@ -1,33 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 #define FIFO_1 "fifo_file1"
 #define FIFO_2 "fifo_file2"
 
-void start_proc1(int fd_write, int fd_read){
-    char arr1[80];
-    while(1){
-        fgets(arr1, 80, stdin);
-        write(fd_write, arr1, strlen(arr1)+1);
+void make_fifo(char *filename){
+    if(mkfifo(filename, 0666) == -1 && errno != EEXIST){
+        perror("Mkfifo");
+        exit(EXIT_FAILURE);
+    }
+}
 
-        read(fd_read, arr1, sizeof(arr1));
-        printf("Proc2: %s\n", arr1);
+void clean(){
+    unlink(FIFO_1);
+    unlink(FIFO_1);
+}
+
+void check_file(int fd){
+    if(fd == -1){
+        perror("Open fifo");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void start_proc1(){
+    char buf[80];
+    int fd_write, fd_read;
+
+    fd_write = open(FIFO_1, O_WRONLY);
+    fd_read = open(FIFO_2, O_RDONLY);
+    check_file(fd_write);
+    check_file(fd_read);
+    
+    printf("Pocess 1 has started. Enter 'quit' toquit\n");
+    while(1){
+        printf("> ");
+        fgets(buf, 80, stdin);
+        buf[strcspn(buf, "\n")] = '\0';
+        if(strcmp(buf, "quit") == 0){
+            break;
+        }
+        write(fd_write, buf, strlen(buf)+1);
+
+        read(fd_read, buf, sizeof(buf));
+        printf("Proc2: %s\n", buf);
     }
     close(fd_read);
     close(fd_write);
 }
 
-void start_proc2(int fd_write, int fd_read){
-    char arr1[80];
+void start_proc2(){
+    char buf[80];
+    int fd_write, fd_read;
+    
+    fd_read = open(FIFO_1, O_RDONLY);
+    fd_write = open(FIFO_2, O_WRONLY);
+    check_file(fd_write);
+    check_file(fd_read);
 
+    printf("Process 2 has started. Waiting for message\n");
     while(1){
-        read(fd_read, arr1, sizeof(arr1));
-        printf("Proc1: %s\n", arr1);
-        fgets(arr1, 80, stdin);
-        write(fd_write, arr1, strlen(arr1)+1);
+        read(fd_read, buf, sizeof(buf));
+        printf("Proc1: %s\n", buf);
+       
+        fgets(buf, 80, stdin);
+        buf[strcspn(buf, "\n")] = '\0';
+        if(strcmp(buf, "quit") == 0){
+            break;
+        }
+        write(fd_write, buf, strlen(buf)+1);
     }
     close(fd_read);
     close(fd_write);
@@ -38,24 +84,17 @@ int main(int argc, char *argv[]){
         printf("Wrong usage: enter 1 or 2\n");
         return 1;
     }
-
-    int fd_write, fd_read;
-    mkfifo(FIFO_1, 0666);
-    mkfifo(FIFO_2, 0666);
+    make_fifo(FIFO_1);
+    make_fifo(FIFO_2);
+    atexit(clean);
 
     if(strcmp(argv[1], "1") == 0){
-        fd_write = open(FIFO_1, O_WRONLY);
-        fd_read = open(FIFO_2, O_RDONLY);
-        start_proc1(fd_write, fd_read);
+        start_proc1();
     }else if(strcmp(argv[1], "2") == 0){
-        fd_write = open(FIFO_2, O_WRONLY);
-        fd_read = open(FIFO_1, O_RDONLY);
-        start_proc2(fd_write, fd_read);
+        start_proc2();
     }else{
         printf("Wrong arguments\n");
         return 1;
     }
-    unlink(FIFO_1);
-    unlink(FIFO_2);
     return 0;
 }
